327ff3dc83667f79fb662e6afe181002b99313ba
jacobhsu <jacob.hsu.tw@gmail.com>
2019-11-11 10:21:55 +0800

    nuxtjs-seo-and-meta-tags

--- hello-nuxt/.eslintrc.js
+++ hello-nuxt/.eslintrc.js
@@ -17,8 +17,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "space-before-function-paren": 0,  // 函式定義時括號前面要不要有空格
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'quotes': ['error', 'single'],  //強制使用單引號 fix error  Strings must use singlequote
-    'semi': ['error', 'always']     //強制使用分號結尾  fix error  Extra semicolon 
+    'vue/multiline-html-element-content-newline': 'off'
   }
 }
